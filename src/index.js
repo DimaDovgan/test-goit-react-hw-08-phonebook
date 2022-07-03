cbfc89db462d906dbce2a4eb6111048932760ec4
DimaDovgan <89789155+DimaDovgan@users.noreply.github.com>
2022-07-03 15:47:17 +0300

    ref

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {store } from './redux/store';
+import { Provider } from "react-redux";
+import {
+  BrowserRouter,
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+        </HashRouter>
+      </Provider>
+      
+  </React.StrictMode>
+);
+
